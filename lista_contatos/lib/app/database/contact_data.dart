073f7ed142cc9c77c50d10aacc6e84ce2fb58d96
Murilo Silva <murilo.alex.silva@gmail.com>
2021-05-19 23:10:36 -0300

    compilando o app

--- lista_contatos/lib/app/database/contact_data.dart
+++ lista_contatos/lib/app/database/contact_data.dart
@@ -2,6 +2,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:async';
 
+/*----Codigo padrão para utilização de banco de dados----*/
+
 final String contactTable = 'contactTable';
 final String idColumn = 'idColumn';
 final String nameColumn = 'nameColumn';
@@ -35,6 +37,74 @@ class ContactDatabase {
           "$phoneColumn TEXT, $imageColumn TEXT)");
     });
   }
+
+  //Salvar Contato
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  //Pegar um contato
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(contactTable,
+        columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn],
+        where: "$idColumn = ?",
+        whereArgs: [id]);
+    if (maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
+
+  //Deletar um contato
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    return await dbContact.delete(
+      contactTable,
+      where: "$idColumn = ?",
+      whereArgs: [id],
+    );
+  }
+
+  //Atualizar um contato
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+    return await dbContact.update(
+      contactTable,
+      contact.toMap(),
+      where: "$idColumn = ?",
+      whereArgs: [contact.id],
+    );
+  }
+
+  //Coletar todos itens da lista de Contatos
+  getAllContacts() async {
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery(
+        "SELECT * FROM $contactTable"); //Estamos selecionando todos os maps da tabela (o * quer dizer todos)
+    List<Contact> listContact = [];
+    for (Map m in listMap) {
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+
+  //Contagem de elementos da tabela
+  Future<int> getNumber() async {
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(
+      await dbContact.rawQuery("SELECT COUNT (*) FROM $contactTable"),
+    );
+  }
+
+  //Fechar o banco de dados
+  Future close() async {
+    Database dbContact = await db;
+    dbContact.close();
+  }
 }
 
 class Contact {
@@ -43,6 +113,8 @@ class Contact {
   String email;
   String phone;
   String image;
+
+  Contact();
 
   Contact.fromMap(Map map) {
     //Pega o mapa e monta o contato
--- lista_contatos/lib/app/modules/home/home_page.dart
+++ lista_contatos/lib/app/modules/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:lista_contatos/app/database/contact_data.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +9,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//Podemos ter somente um data, pois colocamos ele como singleton
+  ContactDatabase contactDatabase = ContactDatabase();
+  List<Contact> contacts = [];
+
+  //Para inicializar sempre que iniciarmos o app
+  @override
+  void initState() {
+    super.initState();
+    contactDatabase.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Contatos'),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.filter_list,
+              size: 32,
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].image != null
+                        ? FileImage(File(contacts[index].image))
+                        : AssetImage("lib/app/images/person.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      contacts[index].name ??
+                          "", //Isso quer dizer que se o nome for salvo vazio, ficará vazio
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].email ??
+                          "", //Isso quer dizer que se o nome for salvo vazio, ficará vazio
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      contacts[index].phone ??
+                          "", //Isso quer dizer que se o nome for salvo vazio, ficará vazio
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
